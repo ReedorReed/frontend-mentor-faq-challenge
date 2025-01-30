7030b826d306cc95f66cf2e3cd6a32addce90c03
Christian Reed <chreed87@gmail.com>
2025-01-30 17:19:00 +0100

    added some margin to the faq questions

--- script.js
+++ script.js
@@ -7,15 +7,33 @@
 // });
 
 // const ques = document.querySelectorAll('.answers');
-const plus = document.querySelectorAll('.plus');
-const minus = document.querySelectorAll('.minus');
 
 function openQues() {
-	let ques = document.getElementsByClassName('answers');
-	let display = document.getComputedStyle('ques').display;
-	if (ques.style.display === '') {
-		ques.style.display = 'block';
-	} else {
-		ques.style.display = ques.style.display === 'none' ? 'block' : 'none';
-	}
+	let qS = document.getElementsByClassName('answers');
+	let plus = document.getElementsByClassName('plus');
+	let minus = document.getElementsByClassName('minus');
+
+	Array.from(qS).forEach((x) => {
+		if (x.style.display === 'none') {
+			x.style.display = 'block';
+		} else {
+			x.style.display = 'none';
+		}
+	});
+
+	Array.from(plus).forEach((y) => {
+		if (y.style.display === 'block') {
+			y.style.display = 'none';
+		} else {
+			y.style.display = 'block';
+		}
+	});
+
+	Array.from(minus).forEach((z) => {
+		if (z.style.display === 'none') {
+			z.style.display = 'block';
+		} else {
+			z.style.display = 'none';
+		}
+	});
 }
