fb8a2a0b8a77dd2da247d815c58ed0c7c7603ebd
Christian Reed <chreed87@gmail.com>
2025-01-30 13:31:42 +0100

    re-did the whole faq page with semantic HTML

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-const faqs = document.querySelectorAll('.faqs');
+// const faqs = document.querySelectorAll('.faqs');
 
-faqs.forEach((faqs) => {
-	faqs.addEventListener('click', () => {
-		faqs.classList.toggle('active');
-	});
-});
+// faqs.forEach((faqs) => {
+// 	faqs.addEventListener('click', () => {
+// 		faqs.classList.toggle('active');
+// 	});
+// });
+
+// const ques = document.querySelectorAll('.answers');
+const plus = document.querySelectorAll('.plus');
+const minus = document.querySelectorAll('.minus');
+
+function openQues() {
+	let ques = document.getElementsByClassName('answers');
+	let display = document.getComputedStyle('ques').display;
+	if (ques.style.display === '') {
+		ques.style.display = 'block';
+	} else {
+		ques.style.display = ques.style.display === 'none' ? 'block' : 'none';
+	}
+}
