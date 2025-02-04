d97be1a4e54fc28b026db687d7c8139bec3271bc
Christian Reed <chreed87@gmail.com>
2025-02-04 08:17:41 +0100

    updated a little

--- script.js
+++ script.js
@@ -1,24 +1,11 @@
-// const faqs = document.querySelectorAll('.faqs');
+const faqs = document.getElementsByClassName('ques');
+const plus = document.getElementsByClassName('plus');
+const minus = document.getElementsByClassName('minus');
+let i;
 
-// faqs.forEach((faqs) => {
-// 	faqs.addEventListener('click', () => {
-// 		faqs.classList.toggle('active');
-// 	});
-// });
-
-// const ques = document.querySelectorAll('.answers');
-
-function openQues() {
-	let qS = document.getElementsByClassName('answers');
-	let plus = document.getElementsByClassName('plus');
-	let minus = document.getElementsByClassName('minus');
-
-	Array.from(qS).forEach((x) => {
-		if (x.style.display === 'none') {
-			x.style.display = 'block';
-		} else {
-			x.style.display = 'none';
-		}
+for (i = 0; i < faqs.length; i++) {
+	faqs[i].addEventListener('click', () => {
+		faqs.classList.toggle('active');
 	});
 
 	Array.from(plus).forEach((y) => {
@@ -37,3 +24,35 @@ function openQues() {
 		}
 	});
 }
+
+// const ques = document.querySelectorAll('.answers');
+
+// function openQues() {
+// 	let qS = document.getElementsByClassName('answers');
+// 	let plus = document.getElementsByClassName('plus');
+// 	let minus = document.getElementsByClassName('minus');
+
+// 	Array.from(qS).forEach((x) => {
+// 		if (x.style.display === 'none') {
+// 			x.style.display = 'block';
+// 		} else {
+// 			x.style.display = 'none';
+// 		}
+// 	});
+
+// 	Array.from(plus).forEach((y) => {
+// 		if (y.style.display === 'block') {
+// 			y.style.display = 'none';
+// 		} else {
+// 			y.style.display = 'block';
+// 		}
+// 	});
+
+// 	Array.from(minus).forEach((z) => {
+// 		if (z.style.display === 'none') {
+// 			z.style.display = 'block';
+// 		} else {
+// 			z.style.display = 'none';
+// 		}
+// 	});
+// }
