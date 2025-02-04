5e5390c9ac0bb2d1049d7a39615cbf32c58b314e
Christian Reed <chreed87@gmail.com>
2025-02-04 13:21:13 +0100

    finished the javascript

--- script.js
+++ script.js
@@ -1,58 +1,33 @@
-const faqs = document.getElementsByClassName('ques');
-const plus = document.getElementsByClassName('plus');
-const minus = document.getElementsByClassName('minus');
-let i;
 
-for (i = 0; i < faqs.length; i++) {
-	faqs[i].addEventListener('click', () => {
-		faqs.classList.toggle('active');
-	});
 
-	Array.from(plus).forEach((y) => {
-		if (y.style.display === 'block') {
-			y.style.display = 'none';
-		} else {
-			y.style.display = 'block';
+document.querySelectorAll('.ques').forEach((question) => {
+	question.addEventListener('click', () => {
+		// Get the related answer and icons
+		let answer = question.nextElementSibling;
+		let plus = question.querySelector('.plus');
+		let minus = question.querySelector('.minus');
+
+		// Check if the clicked answer is already open
+		let isOpen = answer.style.display === 'block';
+
+		// Close all answers first
+		document.querySelectorAll('.answers').forEach((ans) => {
+			ans.style.display = 'none';
+		});
+
+		document.querySelectorAll('.plus').forEach((p) => {
+			p.style.display = 'block';
+		});
+
+		document.querySelectorAll('.minus').forEach((m) => {
+			m.style.display = 'none';
+		});
+
+		// Toggle only if it was NOT already open
+		if (!isOpen) {
+			answer.style.display = 'block';
+			plus.style.display = 'none';
+			minus.style.display = 'block';
 		}
 	});
-
-	Array.from(minus).forEach((z) => {
-		if (z.style.display === 'none') {
-			z.style.display = 'block';
-		} else {
-			z.style.display = 'none';
-		}
-	});
-}
-
-// const ques = document.querySelectorAll('.answers');
-
-// function openQues() {
-// 	let qS = document.getElementsByClassName('answers');
-// 	let plus = document.getElementsByClassName('plus');
-// 	let minus = document.getElementsByClassName('minus');
-
-// 	Array.from(qS).forEach((x) => {
-// 		if (x.style.display === 'none') {
-// 			x.style.display = 'block';
-// 		} else {
-// 			x.style.display = 'none';
-// 		}
-// 	});
-
-// 	Array.from(plus).forEach((y) => {
-// 		if (y.style.display === 'block') {
-// 			y.style.display = 'none';
-// 		} else {
-// 			y.style.display = 'block';
-// 		}
-// 	});
-
-// 	Array.from(minus).forEach((z) => {
-// 		if (z.style.display === 'none') {
-// 			z.style.display = 'block';
-// 		} else {
-// 			z.style.display = 'none';
-// 		}
-// 	});
-// }
+});
