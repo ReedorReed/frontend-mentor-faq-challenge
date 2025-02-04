c7c6028026300aac81fc8a9859cfd0f07d06835b
Christian Reed <Chreed87@gmail.com>
2025-02-04 13:32:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,15 +2,12 @@
 
 document.querySelectorAll('.ques').forEach((question) => {
 	question.addEventListener('click', () => {
-		// Get the related answer and icons
 		let answer = question.nextElementSibling;
 		let plus = question.querySelector('.plus');
 		let minus = question.querySelector('.minus');
 
-		// Check if the clicked answer is already open
 		let isOpen = answer.style.display === 'block';
 
-		// Close all answers first
 		document.querySelectorAll('.answers').forEach((ans) => {
 			ans.style.display = 'none';
 		});
@@ -23,7 +20,6 @@ document.querySelectorAll('.ques').forEach((question) => {
 			m.style.display = 'none';
 		});
 
-		// Toggle only if it was NOT already open
 		if (!isOpen) {
 			answer.style.display = 'block';
 			plus.style.display = 'none';
